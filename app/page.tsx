b46c5e72c085aae1c3756f43310406cdd7962e87
gerardo <gerardo.sosa@farbenmeer.de>
2024-02-24 00:08:52 -0600

    feat: Config codegen and add gql query

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,20 @@
-import { performRequest } from "@/lib/datocms"
-import Image from 'next/image';
+import { getDato } from "@/lib/datocms"
+import Image from 'next/image'
 import bubble from '@/public/bubble.svg'
-
-const PAGE_CONTENT_QUERY = `
-query MyQuery {
-  header {
-    heading
-    id
-    navigationBar {
-      id
-      heading
-    }
-    backgroundAnimation {
-      alt
-      size
-      height
-      width
-      title
-      tags
-    }
-  }
-}`
+import { AllPageDocument } from '@/.graphql/datoTypes'
 
 
 export default async function Home() {
-  const { data } = await performRequest({ query: PAGE_CONTENT_QUERY });
-  console.log(data, data)
-  const { header } = data
+  const { header } = await getDato(AllPageDocument)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header data={header} />
     </main>
-  );
+  )
 }
 
-export function Header({ data }: { data: any }) {
+function Header({ data }: { data: any }) {
   return (
     <header className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-center">{data.heading}</h1>
--- lib/datocms.ts
+++ lib/datocms.ts
@@ -1,22 +1,13 @@
-export const performRequest = async ({ query, variables = {}, includeDrafts = false }: {
-    query: any
-    variables?: Record<string, unknown>
+import { request as graphqlRequest, RequestDocument, Variables } from "graphql-request"
+import { TypedDocumentNode } from "@graphql-typed-document-node/core"
+
+export function getDato<TDocument = any>(
+    document: RequestDocument | TypedDocumentNode<TDocument, Variables>,
+    variables?: Variables,
     includeDrafts?: boolean
-}) => {
-    const response = await fetch("https://graphql.datocms.com/", {
-        headers: {
-            Authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
-            ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
-        },
-        method: "POST",
-        body: JSON.stringify({ query, variables }),
-    });
-
-    const responseBody = await response.json();
-
-    if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}: ${JSON.stringify(responseBody)}`);
-    }
-
-    return responseBody;
+) {
+    return graphqlRequest<TDocument, Variables>("https://graphql.datocms.com/", document, variables, {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN}`,
+        ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
+    })
 }

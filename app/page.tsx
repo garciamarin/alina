0dcc99289423fed3583bb15ef9b31840062265d3
gerardo <gerardo.sosa@farbenmeer.de>
2024-02-24 00:59:02 -0600

    feat: Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import { getDato } from "@/lib/datocms"
 import Image from 'next/image'
 import bubble from '@/public/bubble.svg'
-import { AllPageDocument } from '@/.graphql/datoTypes'
+import { AllPageDocument, AllPageQuery, HeaderRecord, RecordInterface } from '@/.graphql/datoTypes'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Alina Salzer',
+}
 
-export default async function Home() {
+export default async function Page() {
   const { header } = await getDato(AllPageDocument)
 
+  if (!header) {
+    return <div>Page not found</div>
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header data={header} />
@@ -14,11 +21,11 @@ export default async function Home() {
   )
 }
 
-function Header({ data }: { data: any }) {
+function Header({ data }: { data: AllPageQuery['header'] }) {
   return (
     <header className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-center">{data.heading}</h1>
-      <Image src={bubble} alt={data.backgroundAnimation.alt} width={data.backgroundAnimation.width} height={data.backgroundAnimation.height} />
+      <h1 className="text-4xl font-bold text-center">{data?.heading}</h1>
+      {/* <Image src={bubble} alt={data?.backgroundAnimation.alt} width={data?.backgroundAnimation.width} height={data?.backgroundAnimation.height} /> */}
     </header>
   )
 }

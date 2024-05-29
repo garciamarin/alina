f832b071f2050c16f035dfbd3547291a926d557a
gerardo <gerardo.sosa@farbenmeer.de>
2024-05-30 00:58:32 +0200

    feat: Add services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { getDato } from "@/lib/datocms"
-import { AllPageDocument, AllPageQuery } from '@/.graphql/datoTypes'
+import { AllPageDocument } from '@/.graphql/datoTypes'
 import { Metadata } from 'next'
 import Header from "@/components/Header"
 import AboutMe from "@/components/AboutMe"
 import Experience from "@/components/Experience"
 
+import Services from "@/components/Services"
 export const metadata: Metadata = {
   title: 'Alina Salzer',
 }
 
 export default async function Page() {
-  const { header, aboutMe, trainingBlock } = await getDato(AllPageDocument)
+  const { header, aboutMe, trainingBlock, servicesBlock } = await getDato(AllPageDocument)
   if (!header) {
     return <div>Page not found</div>
   }
@@ -19,8 +20,8 @@ export default async function Page() {
       <Header data={header} />
       <AboutMe data={aboutMe} />
       <Experience data={trainingBlock} />
-      <div id="services">Services Section</div>
-      <div id="contact">Contact Section</div>
+      <Services data={servicesBlock} />
     </main>
   )
 }
+
---
+++ components/Services.tsx
@@ -0,0 +1,36 @@
+import { AllPageQuery } from "@/.graphql/datoTypes"
+import image from '@/public/svg/dolmetschen_mund.svg'
+import Image from 'next/image'
+
+
+export default function Services({ data }: { data: AllPageQuery['servicesBlock'] }) {
+    return (
+        <section id="services" className="flex flex-col gap-4 text-center mt-8">
+            {data?.basicContent?.heading &&
+                <div
+                    className="h-[100px] items-center flex"
+                    style={{ background: `url(${data?.basicContent.backgroundImages[0].image?.url}) no-repeat center/cover` }}>
+                    <h2 className="text-3xl font-bold" >
+                        {data?.basicContent.heading}
+                    </h2>
+                </div>
+            }
+            <ul
+                className="flex flex-col gap-4 items-center w-screen"
+                style={{ background: `url(${data?.serviceList[0].animationImages[0].url}) no-repeat center/cover` }}
+            >
+                {
+                    data?.serviceList.map((service) => (
+                        <li key={service.id} className="flex items-center justify-center gap-4">
+                            <span className="font-bold">{service.name}</span>
+                            <Image src={image} alt="" width={50} height={50} />
+                            <div className="w-[300px]">
+                                <span>{service.description}</span>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </section>
+    )
+}

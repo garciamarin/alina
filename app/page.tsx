9e01d390fe2e61e105afc86d97e371389d38e625
gerardo <gerardo.sosa@farbenmeer.de>
2024-05-29 23:44:04 +0200

    feat: Add AboutMe section

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
 import { getDato } from "@/lib/datocms"
-import { AllPageDocument } from '@/.graphql/datoTypes'
+import { AboutMeRecord, AllPageDocument, AllPageQuery } from '@/.graphql/datoTypes'
 import { Metadata } from 'next'
 import Header from "@/components/Header"
+import AboutMe from "@/components/AboutMe"
 
 export const metadata: Metadata = {
   title: 'Alina Salzer',
 }
 
 export default async function Page() {
-  const { header } = await getDato(AllPageDocument)
-
+  const { header, aboutMe } = await getDato(AllPageDocument)
   if (!header) {
     return <div>Page not found</div>
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-4">
       <Header data={header} />
-      <div id='about_me'>About me Section</div>
+      <AboutMe data={aboutMe} />
       <div id="services">Services Section</div>
       <div id="contact">Contact Section</div>
     </main>
---
+++ components/AboutMe.tsx
@@ -0,0 +1,32 @@
+import { AllPageQuery } from "@/.graphql/datoTypes"
+
+export default function AboutMe({ data }: { data: AllPageQuery['aboutMe'] }) {
+    return (
+        <section id="about_me" className="flex flex-col gap-4 text-center">
+            <div
+                className="flex flex-col gap-4 h-[400px] items-center justify-center"
+                style={{ background: `url(${data?.image?.url}) no-repeat center/cover` }}
+            >
+                {data?.title && <h2>{data?.title}</h2>}
+                {data?.subtitle && <h3>{data?.subtitle}</h3>}
+                {data?.introText && <p>{data?.introText}</p>}
+            </ div>
+            <div
+                className="flex flex-col gap-2 items-center justify-center h-[300px]"
+                style={{ background: `url(${data?.meanings?.backgroundImage?.url}) no-repeat center/cover` }}
+            >
+                {data?.meanings?.heading && <h3 className="text-lg">{data?.meanings?.heading}</h3>}
+                <ul className="grid grid-cols-3">
+                    {data?.meanings?.meanings?.map((meaning) => {
+                        return meaning.description &&
+                            <li key={meaning.id}>
+                                <span>{meaning.description}</span>
+                            </li>
+                    }
+                    )}
+                </ul>
+            </div>
+
+        </section >
+    )
+}

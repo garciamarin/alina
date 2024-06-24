7bfd3943c062d13f543b3d77657237f04a149269
gerardo <gerardo.sosa@farbenmeer.de>
2024-06-24 17:05:31 +0200

    feat: Add static logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,21 @@
 import { AllPageQuery } from "@/.graphql/datoTypes"
 import Link from "next/link"
 import Image from "next/image"
+import logo from "@/public/images/logo.png"
 
 export default function Header({ data }: { data: AllPageQuery['header'] }) {
-    const lastAnimationImage = data?.backgroundAnimation.find((image) => image.title === "final")
     return (
         <header className="px-4 lg:px-10 flex justify-between items-start gap-4">
             <div className="md:w-2/3">
-                {lastAnimationImage &&
-                    <Image
-                        className=""
-                        src={lastAnimationImage?.url}
-                        alt={lastAnimationImage.alt || 'understand logo'}
-                        width={lastAnimationImage?.width || 100}
-                        height={lastAnimationImage?.height || 100} />
-                }
+                <Image
+                    className=""
+                    src={logo}
+                    alt={"website's logo"}
+                    width={logo.width || 100}
+                    height={logo.height || 100} />
             </div>
-            <nav className="ml-auto">
-                <ul className="flex space-x-4">
+            <nav className="ml-auto pt-4 lg:pt-8">
+                <ul className="flex lg:space-x-8 space-x-4 font-medium font-mono">
                     {data?.navigationBar.map((navItem) => (
                         <li key={navItem.id}>
                             <Link href={`#${navItem.section}`}>{navItem.heading}</Link>

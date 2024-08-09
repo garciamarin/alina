6981b6be5b3fc16b3dd3828358c8c1ffeacca76b
gerardo <gerardo.sosa@farbenmeer.de>
2024-08-09 14:56:58 +0200

    style: Experience Banner

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,16 +4,15 @@ import Image from "next/image";
 export default function Experience({ data }: { data: AllPageQuery['trainingBlock'] }) {
     return (
         <section id="experience" className="section">
-            <ul className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-4" >
-                <div className="place-content-center flex content-center items-center">
-                    <Image src={data?.experience[0].backgroundImage?.url!} width={50} height={50} alt={data?.experience[0].backgroundImage?.url || ""} />
-                    {data?.experience &&
-                        <h2
-                            className="section-header bg-center bg-no-repeat flex items-center justify-center h-full"
-                        >
-                            {data?.displayName}
-                        </h2>}
-                </div>
+            <ul className="grid grid-cols-1 md:grid-cols-2 gap-10" >
+                {/* <div className="w-[250px] h-[250px]  border-black border-4 self-center"> */}
+                <Image
+                    className="self-start -mt-4 mx-auto"
+                    src={data?.experience[0].backgroundImage?.url!}
+                    width={250}
+                    height={250}
+                    alt={data?.experience[0].backgroundImage?.url || ""} />
+                {/* </div> */}
                 {data?.experience.map((experience) => (
                     <li key={experience.id} className="flex flex-col items-start overflow-visible -m-8 p-8 text-left"
                     >

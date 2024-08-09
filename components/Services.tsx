14c4d7c13c99f6ae505b27a34a5dcec4dec17f68
gerardo <gerardo.sosa@farbenmeer.de>
2024-08-09 14:46:11 +0200

    feat: Services

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,47 +1,63 @@
+"use client"
 import { AllPageQuery, ServicesFragmentFragment } from "@/.graphql/datoTypes"
-import image from '@/public/svg/dolmetschen_mund.svg'
 import bubble from '@/public/svg/bubble.svg'
 import Image from 'next/image'
+import { useState } from "react"
 
 
 export default function Services({ data }: { data: AllPageQuery['servicesBlock'] }) {
+    const [selectedServiceId, setSelectedServiceId] = useState<string | undefined>(undefined)
+    const translateServiceId = data?.serviceList.find(service => service.name === "DOLMETSCHEN")?.id
     return (
-        <section id="services" className="section">
+        <section id="services" className="!w-full section !gap-0">
             {data?.basicContent?.heading &&
-                <div className="flex items-center justify-center"
-                // style={{ background: `url(${data?.basicContent.backgroundImages[0].image?.url})` }}
+                <div className="relative h-[300px] !bg-no-repeat !bg-center"
+                    style={{ background: `url(${data?.basicContent.image?.url!})` }}
                 >
-                    <Image src={data?.basicContent.backgroundImages[0].image?.url!} width={50} height={50} alt={data?.basicContent.backgroundImages[0].image?.url || ""} />
-                    <h2 className="section-header">
+                    <h2 className="section-header absolute top-[30%] md:top-[25%] left-1/2 w-[200px] -translate-x-1/2">
                         {data?.basicContent.heading}
                     </h2>
                 </div>
             }
-            <ul
-                className="flex flex-col gap-4 items-start"
-            // style={{ background: `url(${data?.serviceList[0].animationImages[0].url}) no-repeat center/cover` }}
-            >
-                {
-                    data?.serviceList.map((service) => {
-                        if (service.name === "SPRECHPROBEN") return
-                        return <li key={service.id} className="flex items-center justify-start gap-4 w-full">
-                            <span className="font-bold">{service.name}</span>
-                            <Image src={image} alt="" width={50} height={50} />
-                            <div className="ml-auto text-right">
-                                <span>{service.description}</span>
-                            </div>
-                        </li>
-                    })
-                }
-            </ul>
-            <AudioSamples audioSamples={data?.serviceList.find(service => service.name === "SPRECHPROBEN")!} />
+            <div className="relative !bg-no-repeat !bg-center h-[480px] !bg-contain">
+                <div className="absolute top-0 left-0 w-full h-full !bg-no-repeat !bg-center !bg-cover -z-10"
+                    style={{ background: `url(${data?.basicContent?.backgroundImages?.[0]?.image?.url})` }}
+                />
+                <div>
+                    <ul className="absolute top-1/3 w-[150px] -translate-y-1/2 flex flex-col gap-4">
+                        {
+                            data?.serviceList.map((service) => {
+                                if (service.name === "SPRECHPROBEN") return
+                                return (
+                                    <li key={service.id} className="">
+                                        <button onClick={() => setSelectedServiceId(service.id)} className="flex items-center justify-start gap-8 w-full">
+                                            <span className="font-bold">{service.name}</span>
+                                            <Image src={service.animationImages[4].url} alt="" width={50} height={50} />
+                                        </button>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <div className="absolute top-1/3 left-1/2 w-1/2 -translate-y-1/2 -translate-x-4">
+                        {data?.serviceList.find(service => service.id === selectedServiceId)?.description}
+
+                    </div>
+                    <div className="absolute top-1/2 left-1/2 w-1/2 -translate-y-8 -translate-x-4">
+                        {selectedServiceId === translateServiceId
+                            &&
+                            <AudioSamples audioSamples={data?.serviceList.find(service => service.name === "SPRECHPROBEN")!} />
+                        }
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
 
 function AudioSamples({ audioSamples }: { audioSamples: ServicesFragmentFragment['serviceList'][0] }) {
     return (
-        <div className="section">
+        <div>
             <div className="flex items-center justify-center">
                 <Image src={bubble} width={50} height={50} alt={""} />
                 <h2 className="section-header">{audioSamples.name}</h2>
@@ -49,7 +65,7 @@ function AudioSamples({ audioSamples }: { audioSamples: ServicesFragmentFragment
             <ul>
                 {audioSamples.audioList.map(sample => {
                     return (
-                        <li className="flex items-center gap-4 justify-center" key={sample.displayName}>
+                        <li className="flex items-center  justify-center" key={sample.displayName}>
                             <span>{sample.displayName}</span>
                             <audio controls>
                                 <source src={sample.audio?.url} type="audio/mpeg" />

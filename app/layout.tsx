434cad2f866b84b6f2d4bca4b3161b055e123d50
gerardo <gerardo.sosa@farbenmeer.de>
2024-09-12 17:21:32 +0200

    feat: Allow smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="text-justify">
+    <html lang="en" className="text-justify scroll-smooth">
       <body className={`${urbanist.variable} ${bahnSchrift.variable} font-sans bg-[#edebec]`}>
         {children}
       </body >

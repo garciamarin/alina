16aabecdb8f4350e56d10f9ab99a8ab9887889a7
gerardo <gerardo.sosa@farbenmeer.de>
2024-10-10 16:26:49 +0200

    feat: Play audios on click on images

--- components/AudioSamples.tsx
+++ components/AudioSamples.tsx
@@ -1,6 +1,8 @@
-import { AudioListDocument } from "@/.graphql/datoTypes"
+import {
+    AudioListDocument, AudioListQuery
+} from "@/.graphql/datoTypes"
 import { getDato } from "@/lib/datocms"
-import { cache } from "react";
+import { cache, useRef, useState } from "react";
 
 const getAudios = cache(
     async () => {
@@ -13,17 +15,43 @@ export async function AudioSamples() {
     const audioList = await getAudios();
 
     return (
-        <ul className="mt-8 grid grid-cols-2 gap-4 justify-between">
-            {audioList?.audio.map(sample => {
-                return (
-                    <li className="flex flex-col items-center  justify-center gap-1" key={sample.displayName}>
-                        <p className="font-bold">{sample.displayName}</p>
-                        <audio controls>
-                            <source src={sample.audio?.url} type="audio/mpeg" />
-                        </audio>
-                    </li>
-                )
-            })}
+        <ul className="mt-8 grid grid-cols-2 gap-6 md:gap-16">
+            {
+                audioList?.audio.map(sample => <AudioSample key={sample.displayName} sample={sample} />)
+            }
         </ul >
+    )
+}
+
+function AudioSample({ sample }: { sample: NonNullable<AudioListQuery['audioList']>['audio'][number] }) {
+    const audioRef = useRef<null | HTMLAudioElement>(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const toggleAudio = () => {
+        if (audioRef.current) {
+            if (isPlaying) {
+                audioRef.current.pause();
+            } else {
+                audioRef.current.play();
+            }
+            setIsPlaying(!isPlaying);
+        }
+    };
+
+    return (
+        <li>
+            <button
+                onClick={(toggleAudio)}
+                className="border-4 border-red-500 relative w-full h-auto cursor-pointer bg-contain bg-no-repeat bg-center border-none p-0 flex items-center justify-center outline-none"
+                style={{ backgroundImage: `url(${sample.animationImages[0].url})` }}
+            >
+                <div className="text-white text-xl p-16 opacity-30 hover:opacity-80" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+                    {isPlaying ? '⏸️' : '▶️'}
+                </div>
+                <audio ref={audioRef}>
+                    <source src={sample.audio?.url} type="audio/mpeg" />
+                </audio>
+            </button>
+        </li>
     )
 }
